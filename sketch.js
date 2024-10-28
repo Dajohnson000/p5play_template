449ed2532d56a2f4efbb8efd9f71cc1d72d87000
Dajohnson000 <daj22c@fsu.edu>
2024-10-27 20:57:39 -0400

    Checking if it is true

--- sketch.js
+++ sketch.js
@@ -1,32 +1,45 @@
 let frown;
 let smiley;
-let recipe = 'recipe: ';
+let recipe = '';
 let dizzy;
 let mad;
+const answer = ' dizzy';
+let check;
+let youWon = false;
 function setup() {
-    let canvas = new Canvas("fullscreen");
-    textFont("Courier", 24);
-    rectMode(CENTER);
-    frown = new Sprite();
-    frown.x = 50;
-    smiley = new Sprite();
-    smiley.x = 125;
-    dizzy = new Sprite();
-    dizzy.x = 200;
-    mad = new Sprite();
-    mad.x = 275;
-    if (frown.mouse.pressing){
-        recipe = recipe + 'frown';
-       }
+  let canvas = new Canvas("fullscreen");
+  textFont("Courier", 24);
+  rectMode(CENTER);
+  frown = createSprite();
+  frown.x = 50;
+  smiley = new Sprite();
+  smiley.x = 125;
+  dizzy = new Sprite();
+  dizzy.x = 200;
+  mad = new Sprite();
+  mad.x = 275;
+  check = new Sprite();
 }
 
 function draw() {
-   clear(); 
-   text('frown', 20, height/2 - 100);
-   text('smiley', 100, height/2 - 100);
-   text('dizzy', 190, height/2 - 100);
-   text('mad',270, height/2 - 100);
-   text(recipe, 100, 100);
-
+  clear();
+  text("frown", 20, height / 2 - 100);
+  text("smiley", 100, height / 2 - 100);
+  text("dizzy", 190, height / 2 - 100);
+  text("mad", 270, height / 2 - 100);
+  text(recipe, 100, 100);
+  text("check", 750, height / 2 - 100);
+  if (frown.mouse.pressed()) recipe = recipe + " frown";
+  if (smiley.mouse.pressed()) recipe = recipe + " smiley";
+  if (dizzy.mouse.pressed()) recipe = recipe + " dizzy";
+  if (mad.mouse.pressed()) recipe = recipe + " mad";
+  text(answer, 150, 125);
 }
+function keyPressed() { 
+    console.log(recipe);
+    console.log(answer);
+    youWon = recipe === answer ? true : false;
+    console.log(youWon);
+  }
+
 
